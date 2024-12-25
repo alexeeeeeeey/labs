b8f759174d8aaece239017c5b958ef9628ecf156
alexeeeeeeey <alexey.shaganov1@gmail.com>
2024-12-25 12:52:33 +0300

    infsec lab 2 correct

--- infsec/lab2/main.py
+++ infsec/lab2/main.py
@@ -1,17 +1,11 @@
-class MultiplyError(Exception):
-    pass
-
-
 def matrix_sum(str_length: int) -> list[int]:
     """Создаёт матрицу контрольной суммы для заданной длины строки"""
     matrix = []
 
     bit_position = 1
     while bit_position <= str_length:
-        # Изначально строка заполняется нулями
         row = [0] * str_length
 
-        # Проставляем 1 в позиции, где бит участвует
         for i in range(bit_position - 1, str_length, 2 * bit_position):
             row[i : i + bit_position] = [1] * min(bit_position, str_length - i)
 
@@ -21,51 +15,58 @@ def matrix_sum(str_length: int) -> list[int]:
     return matrix
 
 
-def gen_code(seq: list) -> list[int]:
-    """Генерирует код с контрольными битами"""
-    m = len(seq)  # Количество информационных битов
-    r = 0  # Число проверочных битов
+def gen_control_vector(seq_with_more_bits: list, matrix: list[list[int]]):
 
-    while 2**r < m + r + 1:
-        r += 1
+    contorl_vector = []
 
-    total_bits = m + r
-    encoded = [None] * total_bits
+    for i in matrix:
+        xored_vector = []
+        for (i, j) in zip(i, seq_with_more_bits):
+            xored_vector.append(i * j)
+        contorl_vector.append(sum(xored_vector) % 2)
+    
+    return contorl_vector
 
-    # Заполняем проверочные биты значениями None
+
+def insert_control_vector(contol_vector: list, seq: list) -> list:
+    new_seq = []
+    j = 0
+    k = 0
+    seq_copy = seq.copy()
+    total_bits = len(seq) + len(contol_vector)
     for i in range(total_bits):
-        if (i + 1) & i == 0:  # Позиции 1, 2, 4, 8...
-            encoded[i] = 0
+        if i + 1 == 2**j:
+            new_seq.append(contol_vector[k])
+            k += 1
+            j += 1
         else:
-            encoded[i] = seq.pop(0)
+            new_seq.append(seq_copy.pop(0))
 
-    # Рассчитываем проверочные биты
-    for i in range(r):
-        parity_index = 2**i - 1
-        parity = 0
-        for j in range(parity_index, total_bits, 2 ** (i + 1)):
-            parity ^= sum(encoded[j : j + 2**i])
-        encoded[parity_index] = parity % 2
+    return new_seq
+
+
+def gen_code(seq: list, matrix: list[list[int]]) -> list[int]:
+    """Генерирует код с контрольными битами"""
+    r = len(matrix[0])
+    cb = r - len(seq)
+
+    seq_with_zero_sums = insert_control_vector([0] * cb, seq)
+
+    control_vector = gen_control_vector(seq_with_zero_sums, matrix)
+    print("Контрольный вектор: ", control_vector)
+
+    encoded = insert_control_vector(control_vector, seq)
 
     return encoded
 
 
-def check_code(seq: list):
-    """функция проверки на наличие ошибок"""
-    n = len(seq)
-    r = 0
+def check_code(seq: list, matrix: list[list[int]]) -> int:
+    """Функция проверки на наличие ошибок"""
 
-    while 2**r < n:
-        r += 1
 
-    error_pos = 0
-    for i in range(r):
-        parity_index = 2**i - 1
-        parity = 0
-        for j in range(parity_index, n, 2 ** (i + 1)):
-            parity ^= sum(seq[j : j + 2**i])
-        if parity % 2 != 0:
-            error_pos += 2**i
+    control_vector = gen_control_vector(seq, matrix)
+
+    error_pos = int("".join(map(str, control_vector[::-1])), 2)
 
     return error_pos
 
@@ -103,35 +104,38 @@ def main():
                 continue
             break
 
+        if optype in [GEN_CODE, CHECK_CODE]:
+            m = len(seq)  # Количество информационных битов
+            r = 0  # Число проверочных битов
+
+            while 2**r < m + r + 1:
+                r += 1
+
+            matrix = matrix_sum(m + r)
+
         # генерация нового кода.
         if optype == GEN_CODE:
-
-            matrix = matrix_sum(len(seq))
-
             print("==== MATRIX ====")
             for line in matrix:
                 print(line)
             print("== END MATRIX ==")
 
-            code = gen_code(seq)
+            code = gen_code(seq, matrix)
 
             print(
-                f"Сгенерированный код имеет длину {len(code)}:\n"
+                f"Сгенерированный код:\n"
                 f"{''.join(map(str, code))}"
             )
 
         # проверка существующего кода.
         elif optype == CHECK_CODE:
-            try:
-                error_pose = check_code(seq)
+            error_pose = check_code(seq, matrix)
 
-                if error_pose == 0:
-                    print("Ошибок не найдено.")
-                else:
-                    print(" " * (12 + error_pose) + "^")
-                    print(f"Ошибка в позиции {error_pose} ")
-            except MultiplyError:
-                print("В коде больше 2 ошибок")
+            if error_pose == 0:
+                print("Ошибок не найдено.")
+            else:
+                print(" " * (12 + error_pose) + "^")
+                print(f"Ошибка в позиции {error_pose} ")
 
 
 if __name__ == "__main__":
